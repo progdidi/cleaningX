fdeabe22201f27d1fd569c76df7a339dd8fcce7d
Diana Sosnovskaya <progdidi@gmail.com>
2023-12-05 17:34:56 +0100

    feat: add header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
-$(function(){
-   
-});
+//active buttons
+const headerBtns = document.querySelectorAll('.header__btn');
+headerBtns.forEach((btn) => {
+    btn.addEventListener('mouseover', () => {
+        btn.classList.add('active-btn');
+    })
+})
+
+
+//mobile menu
+const menuBtn = document.querySelector('.menu-btn');
+const menuList = document.querySelector('.menu__list');
+menuBtn.addEventListener('click', () => {
+    menuList.classList.toggle('active');
+})
